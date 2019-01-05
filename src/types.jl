0bc08ece5d9ea23dee92d818acee1438011a964f
Harrison Grodin <harrison.grodin@gmail.com>
2019-01-05 14:11:58 -0500

    Use two-way dictionary for constant pool

--- src/types.jl
+++ src/types.jl
@@ -24,13 +24,22 @@ Variable() = Variable(VARIABLE_COUNTER[] += 1)
 
 
 struct TermSet{T}
-    pool::Vector{T}
-    TermSet{T}() where {T} = new{T}(T[])
+    insert::Dict{T,Index}
+    lookup::Dict{Index,T}
+    count::Ref{Index}
+    TermSet{T}() where {T} = new{T}(Dict{T,Index}(), Dict{Index,T}(), Ref{Index}(0))
 end
 Base.broadcastable(ts::TermSet) = Ref(ts)
 function Base.getindex(ts::TermSet, x::Node)
     x.kind === VARIABLE && return Variable(x.index)
-    x.kind === CONSTANT && return ts.pool[x.index]
+    x.kind === CONSTANT && return ts.lookup[x.index]
+end
+function Base.push!(ts::TermSet, x)
+    haskey(ts.insert, x) && return Node(CONSTANT, ts.insert[x])
+    index = (ts.count[] += 1)
+    ts.insert[x] = index
+    ts.lookup[index] = x
+    Node(CONSTANT, index)
 end
 
 
@@ -38,7 +47,7 @@ struct Term{T}
     tree::Tree
     set::TermSet{T}
 end
-(ts::TermSet)(ex)  = Term(expr_to_term(ts, ex), ts)
+(ts::TermSet)(ex) = Term(expr_to_term(ts, ex), ts)
 Base.convert(::Type{Expr}, t::Term) = term_to_expr(t)
 
 Base.:(==)(s::Term, t::Term) = s.set === t.set && convert(Expr, s) == convert(Expr, t)
--- src/util.jl
+++ src/util.jl
@@ -1,12 +1,8 @@
-expr_to_term(ts::TermSet, ex::Expr)    = Tree(ex.head, expr_to_term.(ts, ex.args))
-expr_to_term(ts::TermSet, x)           = Tree(:POOL, [expr_to_node(ts, x)])
+expr_to_term(ts::TermSet, ex::Expr) = Tree(ex.head, expr_to_term.(ts, ex.args))
+expr_to_term(ts::TermSet, x)        = Tree(:POOL, [expr_to_node(ts, x)])
 
 expr_to_node(ts::TermSet, x::Variable) = Node(VARIABLE, x.id)
-function expr_to_node(ts::TermSet, x)
-    index = findfirst(isequal(x), ts.pool)
-    index === nothing && (index = length(push!(ts.pool, x)))
-    Node(CONSTANT, index)
-end
+expr_to_node(ts::TermSet, x)           = push!(ts, x)
 
 
 term_to_expr(t::Term) = term_to_expr(t.set, t.tree)
