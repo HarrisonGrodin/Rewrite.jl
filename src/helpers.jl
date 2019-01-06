b7a7d9f3cb6e55d0b0c1ac05f62d8996dc47d06f
Harrison Grodin <harrison.grodin@gmail.com>
2019-01-05 19:58:13 -0500

    Remove tree function

--- src/helpers.jl
+++ src/helpers.jl
@@ -1,13 +1,10 @@
-export tree
 export head, children
 
 
-@inline tree(t::Term) = t.tree
-
 @inline head(x::Node) = x
 @inline head(t::Branch) = t.head
-@inline head(t::Term) = head(tree(t))
+@inline head(t::Term) = head(t.tree)
 
 @inline children(x::Node) = Tree[]
 @inline children(t::Branch) = t.args
-@inline children(t::Term) = children(tree(t))
+@inline children(t::Term) = children(t.tree)
