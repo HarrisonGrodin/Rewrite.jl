7f66bf8b9277c9d7d193bb1d0e2221e56275f75a
Harrison Grodin <harrison.grodin@gmail.com>
2019-01-07 14:10:47 -0500

    Introduce promotion rules and additional constructors

--- src/term.jl
+++ src/term.jl
@@ -2,12 +2,16 @@ export Variable, Term
 
 
 mutable struct Variable end
+Base.promote_rule(::Type{Variable}, T::Type) = Union{Variable, T}
+Base.promote_rule(::Type{Variable}, ::Type{Any}) = Any
 
 
 struct Term{T}
     head::T
     args::Vector{Term{T}}
 end
+Term(head::T) where {T} = Term{T}(head, T[])
+Term(head::T, args::Vector{Term{U}}) where {T,U} = Term{promote_type(T,U)}(head, args)
 
 Base.:(==)(s::Term, t::Term) = (s.head, s.args) == (t.head, t.args)
 
@@ -15,3 +19,5 @@ Base.convert(::Type{Term{T}}, ex) where {T} = expr_to_term(T, ex)
 Base.convert(::Type{Term{T}}, t::Term) where {T} = Term{T}(t.head, t.args)
 
 Base.convert(::Type{Expr}, t::Term) = term_to_expr(t)
+
+Base.promote_rule(::Type{Term{S}}, ::Type{Term{T}}) where {S,T} = Term{promote_type(S,T)}
