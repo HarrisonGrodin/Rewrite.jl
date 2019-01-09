4e1bfc2e22f1d6159ed123ead1f17444cfd8fbee
Harrison Grodin <harrison.grodin@gmail.com>
2019-01-09 13:35:01 -0500

    Implement @term macro (#9)
    * Implement @term macro
    
    * Implement @term show
    
    * Add @MasonProtter to acknowledgements
    
    * Add tests
    
    * Remove special casing for anonymous functions
    
    Output led to a different term altogether.

--- src/term.jl
+++ src/term.jl
@@ -1,4 +1,4 @@
-export Term
+export Term, @term
 export is_branch, root, children
 
 
@@ -23,4 +23,36 @@ function Base.map(f, t::Term)
     expr = Expr(root(t))
     append!(expr.args, map(t -> (f(t)::Term).x, children(t)))
     convert(Term, expr)
+end
+
+
+macro term(ex)
+   :(Term($(_term(ex))))
+end
+function _term(ex)
+    isa(ex, Expr) || return esc(ex)
+    ex.head === :$ && return esc(ex.args[1])
+    ex.head === :. && return esc(ex)
+    return :(Expr($(Meta.quot(ex.head)), $(_term.(ex.args)...)))
+end
+
+
+function _show_term(f::Function)
+    # Inspired by: `show(::IO, ::Function)`
+    ft = typeof(f)
+    mt = ft.name.mt
+    Base.is_exported_from_stdlib(mt.name, mt.module) && return mt.name
+    return :($(nameof(mt.module)).$(mt.name))
+end
+function _show_term(ex::Expr)
+    ex′ = Expr(ex.head)
+    append!(ex′.args, _show_term.(ex.args))
+    ex′
+end
+_show_term(x::Symbol) = Meta.quot(x)
+_show_term(x) = x
+function Base.show(io::IO, t::Term)
+    macro_call = Expr(:macrocall, Symbol("@term"), nothing, _show_term(t.x))
+    repr = sprint(show, macro_call)[9:end-1]
+    print(io, "@term(", repr, ")")
 end
--- test/term.jl
+++ test/term.jl
@@ -3,6 +3,30 @@ using Test
 
 
 @testset "construction" begin
+    x = Variable()
+    k = 7.8
+
+    t1 = @term(x)
+    @test root(t1) === x
+    @test isempty(children(t1))
+
+    t2 = @term(k)
+    @test root(t2) === k
+    @test isempty(children(t2))
+
+    t3 = @term("test")
+    @test root(t3) == "test"
+    @test isempty(children(t3))
+
+    t4 = @term(x^2 + k)
+    @test root(t4) === :call
+    @test children(t4) == [@term(+), @term(x^2), @term(k)]
+
+    t5 = @term(x^$(1+1) + k)
+    @test t4 == t5
+end
+
+@testset "conversion" begin
     @test convert(Term, :(x + 2y)) == convert(Term, :(x + 2y))
     @test convert(Term, :(x + 2y)) ≠ convert(Term, :(x + 3y))
     @test convert(Term, :(x + 2y)) ≠ convert(Term, :(x + 2z))
@@ -30,6 +54,26 @@ using Test
 end
 
 @testset "equality" begin
-    @test convert(Term, 0.0) == convert(Term, -0.0)
-    @test !isequal(convert(Term, 0.0), convert(Term, -0.0))
+    @test @term(0.0) == @term(-0.0)
+    @test !isequal(@term(0.0), @term(-0.0))
+end
+
+@testset "show" begin
+    @test sprint(show, @term(2)) == "@term(2)"
+    @test sprint(show, @term(-3.7)) == "@term(-3.7)"
+    @test sprint(show, @term("test")) == "@term(\"test\")"
+    @test sprint(show, @term(:x)) == "@term(:x)"
+
+    let x = 1
+        @test sprint(show, @term([x, :x])) == "@term([1, :x])"
+    end
+
+    let x = Variable()
+        @test sprint(show, @term([x, :x])) == "@term([$x, :x])"
+    end
+
+    @test sprint(show, @term(sin(π) + (3 - 5))) == "@term(sin($π) + (3 - 5))"
+
+    @test sprint(show, @term(Base.Broadcast.materialize(1, 2))) ==
+        "@term(Broadcast.materialize(1, 2))"
 end
