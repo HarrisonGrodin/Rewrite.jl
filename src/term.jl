9990010f6fd7fdc1ea629ef01acba64f3a0e0786
Harrison Grodin <harrison.grodin@gmail.com>
2019-01-18 15:50:40 -0500

    Replace is_branch(t) with !isleaf(t)

--- src/term.jl
+++ src/term.jl
@@ -1,14 +1,14 @@
 export Term, @term
-export is_branch, root, children
+export isleaf, root, children
 
 
 struct Term
     x
 end
 
-@inline is_branch(t::Term) = isa(t.x, Expr)
-@inline root(t::Term)     = is_branch(t) ? t.x.head                 : t.x
-@inline children(t::Term) = is_branch(t) ? convert.(Term, t.x.args) : Term[]
+@inline isleaf(t::Term) = !isa(t.x, Expr)
+@inline root(t::Term)     = !isleaf(t) ? t.x.head                 : t.x
+@inline children(t::Term) = !isleaf(t) ? convert.(Term, t.x.args) : Term[]
 
 Base.convert(::Type{Term}, t::Term) = t
 Base.convert(::Type{Term}, x) = Term(x)
