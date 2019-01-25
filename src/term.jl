7b9f962eb29fc3fdfa1435404a4c23b94b88e817
Harrison Grodin <harrison.grodin@gmail.com>
2019-01-25 00:21:51 -0500

    Redesign API to match Indexing interface (#14)
    * Implement additional Base methods
    
    * Remove children in favor of Base.collect
    
    * Revert "Remove children in favor of Base.collect"
    
    This reverts commit 94e55b1cbb857f9b4f41afcbca3600b9d85abc39.
    
    * Remove iteration
    
    * Improve coverage

--- src/term.jl
+++ src/term.jl
@@ -10,14 +10,6 @@ end
 @inline root(t::Term) = isleaf(t) ? t.x : t.x.head
 @inline children(t::Term) = [t[i] for i ∈ eachindex(t)]
 
-function Base.getindex(t::Term, i)
-    @boundscheck isleaf(t) && throw(BoundsError(t, i))
-    @boundscheck checkbounds(Bool, t.x.args, i) || throw(BoundsError(t, i))
-    return convert(Term, t.x.args[i])
-end
-Base.getindex(t::Term, inds...) = foldl(getindex, inds; init=t)
-Base.eachindex(t::Term) = isleaf(t) ? Base.OneTo(0) : eachindex(t.x.args)
-
 Base.convert(::Type{Term}, t::Term) = t
 Base.convert(::Type{Term}, x) = Term(x)
 Base.convert(::Type{Expr}, t::Term) = t.x
@@ -25,11 +17,21 @@ Base.convert(::Type{Expr}, t::Term) = t.x
 Base.:(==)(s::Term, t::Term) = s.x == t.x
 Base.isequal(s::Term, t::Term) = isequal(s.x, t.x)
 
+Base.eachindex(t::Term) = isleaf(t) ? Base.OneTo(0) : eachindex(t.x.args)
+Base.firstindex(t::Term) = first(eachindex(t))
+Base.lastindex(t::Term) = last(eachindex(t))
+
+@inline function Base.getindex(t::Term, i)
+    @boundscheck checkindex(Bool, eachindex(t), i) || throw(BoundsError(t, i))
+    return convert(Term, t.x.args[i])
+end
+Base.getindex(t::Term, inds...) = foldl(getindex, inds; init=t)
+
 function Base.map(f, t::Term)
     isa(t.x, Expr) || return t
 
     expr = Expr(root(t))
-    append!(expr.args, map(t -> (f(t)::Term).x, children(t)))
+    append!(expr.args, map(_unwrap ∘ f, children(t)))
     convert(Term, expr)
 end
 
