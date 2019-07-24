47c514e28a232a1c557e8730978cd79d7b5371f1
Harrison Grodin <harrison.grodin@gmail.com>
2019-07-24 13:33:50 -0400

    Compilation to Expr (#23)
    * Rename compile to matcher
    
    * Add matcher compilation
    
    * Add rewriter compilation
    
    * Fix code ordering
    
    * Fix bug
    
    * Update API to include rules compilation
    
    * Improve coverage

--- src/theory.jl
+++ src/theory.jl
@@ -1,85 +1,53 @@
-export @theory, @term, @rules, @rewrite
-export Theory, Term, Rules, rewrite
+export @theory!, @term, @rules, @rewrite
 
 
-struct Theory
-    name::Symbol
-    d::Dict{Σ,AbstractTheory}
-end
-Base.show(io::IO, th::Theory) = print(io, th.name)
+const THEORY = Dict{Σ,AbstractTheory}()
 
+macro theory!(theories)
+    @assert isa(theories, Expr) && theories.head === :block
+    Base.remove_linenums!(theories)
 
-struct Term
-    th::Theory
-    t::Union{Variable,AbstractTerm}
-end
-Base.:(==)(s::Term, t::Term) = s.th == t.th && s.t == t.t
-Base.hash(t::Term, h::UInt) = hash(t.t, hash(t.th, hash(Term, h)))
-function Base.show(io::IO, t::Term)
-    term_expr = convert(Expr, t.t)
-    Base.show_call(io, :call, Symbol("@term"), [t.th.name, term_expr], 0)
+    for line ∈ theories.args
+        @assert line.head === :call && line.args[1] === :(=>)
+        THEORY[line.args[2]] = Base.eval(__module__, line.args[3])
+    end
+
+    nothing
 end
 
-function _to_theory(th::Theory, expr, strict)
+function _to_theory(th, expr; strict=false)
     (root, args) = if isa(expr, Symbol)
         (expr, [])
     elseif isa(expr, Expr)
         expr.head === :call || throw(ArgumentError("invalid non-call Expr"))
         (expr.args[1], expr.args[2:end])
-    elseif isa(expr, Term)
-        expr.th == th || throw(ArgumentError("cannot combine different theories: $(expr.th) and $th"))
-        return expr.t
     elseif isa(expr, Union{Variable,AbstractTerm})
         return expr
     else
         throw(ArgumentError("invalid expression: $(repr(expr))"))
     end
 
-    root_theory = haskey(th.d, root) ? th.d[root] :
+    strict = false  # FIXME
+    root_theory = haskey(th, root) ? th[root] :
         strict ? throw(ArgumentError("$root undefined in theory")) : FreeTheory()
 
     for i ∈ eachindex(args)
-        args[i] = _to_theory(th, args[i], strict)
+        args[i] = _to_theory(th, args[i]; strict=strict)
     end
 
     term(root_theory, root, args)
 end
 
-(th::Theory)(expr; strict=false) = Term(th, _to_theory(th, expr, strict))
 
-macro term(th, expr)
-    :($(esc(th))($(Meta.quot(expr))))
+function Base.show(io::IO, t::Union{Variable, AbstractTerm})
+    term_expr = convert(Expr, t)
+    Base.show_call(io, :call, Symbol("@term"), [term_expr], 0)
 end
 
-macro theory(name, body)
-    @assert isa(name, Symbol)
-    @assert isa(body, Expr) && body.head === :block
-    Base.remove_linenums!(body)
-
-    for i ∈ eachindex(body.args)
-        line = body.args[i]
-        @assert line.head === :call
-        line.args[2] = Meta.quot(line.args[2])
-        body.args[i] = esc(line)
-    end
-
-    :($(esc(name)) = Theory($(Meta.quot(name)), Dict($(body.args...))))
+macro term(expr)
+    :(_to_theory(THEORY, $(Meta.quot(expr))))
 end
 
-
-struct Rules
-    name::Symbol
-    th::Theory
-    rw::Rewriter
-    Rules(name, th) = new(name, th, Rewriter())
-end
-Rules(name, th, rules) = push!(Rules(name, th), rules...)
-Base.show(io::IO, rs::Rules) = print(io, rs.name)
-function Base.push!(rs::Rules, (p, b)::Pair{Term})
-    rs.th == p.th || throw(ArgumentError("rules and rule are from different theories: $(rs.th) and $(p.th)"))
-    push!(rs.rw, p.t => b)
-    rs
-end
 
 function _rules_replace!(ex, vars)
     isa(ex, Symbol) && return get(vars, ex, ex)
@@ -91,47 +59,38 @@ function _rules_replace!(ex, vars)
     end
     return ex
 end
-function _clean(ex, th, vars)
-    ex′ = Meta.quot(_rules_replace!(ex, vars))
-    :($(esc(th))($ex′; strict=true))
+function _clean(ex, vars)
+    ex′ = _rules_replace!(ex, vars)
+    _to_theory(THEORY, ex′; strict=true)
 end
-macro rules(name, th, varnames, body)
-    @assert isa(th, Symbol)
+macro rules(name, varnames, body)
     @assert isa(name, Symbol)
     @assert isa(varnames, Expr) && varnames.head === :vect
     @assert all(isa(x, Symbol) for x ∈ varnames.args)
+
     @assert isa(body, Expr) && body.head === :block
     Base.remove_linenums!(body)
 
     vars = Dict(x => Variable() for x ∈ varnames.args)
-    var_exprs = Expr(:block, (:($x = $v) for (x, v) ∈ vars)...)
 
-    args = similar(body.args, Expr)
+    rw = Rewriter()
     for i ∈ eachindex(body.args)
         line = body.args[i]
         @assert line.head === :(:=)
-        lhs, rhs = _clean.(line.args, Ref(th), Ref(vars))
-        args[i] = :($lhs => replace($rhs))
+        lhs, rhs = _clean.(line.args, Ref(vars))
+        push!(rw, lhs => replace(rhs))
     end
 
-    let_expr = Expr(:let, var_exprs, :(Rules($(Meta.quot(name)), $(esc(th)), [$(args...)])))
-    return :($(esc(name)) = $let_expr)
+    fn, expr = compile(rw)
+
+    struct_name = gensym(name)
+    return quote
+        $expr
+        $(esc(name))(t) = $fn(t)
+    end
 end
 
 
-_wrap_theory(th) = Base.Fix1(_wrap_theory, th)
-_wrap_theory(th, d) = Dict(k => th(v) for (k, v) ∈ d)
-function match(s::Term, t::Term)
-    s.th == t.th || throw(ArgumentError("pattern and subject are from different theories: $(s.th) and $(t.th)"))
-    LazyMap(_wrap_theory(s.th), match(s.t, t.t))
-end
-
-replace(t::Term) = Base.Fix1(replace, t.t)
-function rewrite(rs::Rules, t::Term)
-    rs.th == t.th || throw(ArgumentError("rules and term are from different theories: $(rs.th) and $(t.th)"))
-    Term(t.th, rewrite(rs.rw, t.t))
-end
-
-macro rewrite(rs, ex)
-    :(rewrite($(esc(rs)), $(esc(rs)).th($(Meta.quot(ex)))))
+macro rewrite(rw, expr)
+    :($(esc(rw))(_to_theory(THEORY, $(Meta.quot(expr)))))
 end
--- test/rewrite.jl
+++ test/rewrite.jl
@@ -1,101 +1,100 @@
 @testset "empty" begin
-    rw = Rules(:Demo, Example)
-    @test rewrite(rw, a()) == a()
-    @test rewrite(rw, f(a())) == f(a())
+    @rules Demo [] begin end
+    @test Demo(a()) == a()
+    @test Demo(f(a())) == f(a())
 end
 
 @testset "free, simple" begin
-    rw = Rules(:Demo, Example, [f(x) => replace(g(x))])
-    @test rewrite(rw, f(a())) == g(a())
-    @test rewrite(rw, h(f(a()))) == h(g(a()))
-    @test rewrite(rw, g(a())) == g(a())
-    @test rewrite(rw, h(a())) == h(a())
+    @rules Demo [x] begin
+        f(x) := g(x)
+    end
+    @test Demo(f(a())) == g(a())
+    @test Demo(h(f(a()))) == h(g(a()))
+    @test Demo(g(a())) == g(a())
+    @test Demo(h(a())) == h(a())
 end
 
 @testset "free, complex" begin
     @testset "orthogonal" begin
-        rw = Rules(:Demo, Example, [
-            f(x)       => replace(g(x)),
-            h(x, x, y) => replace(p(x, y)),
-        ])
+        @rules Demo [x, y] begin
+            f(x)       := g(x)
+            h(x, x, y) := p(x, y)
+        end
 
-        @test rewrite(rw, f(a())) == g(a())
-        @test rewrite(rw, h(a(), a(), b())) == p(a(), b())
-        @test rewrite(rw, h(a(), b(), b())) == h(a(), b(), b())
+        @test Demo(f(a())) == g(a())
+        @test Demo(h(a(), a(), b())) == p(a(), b())
+        @test Demo(h(a(), b(), b())) == h(a(), b(), b())
     end
 
     @testset "overlapping" begin
-        rw = Rules(:Demo, Example, [
-            f(x)    => replace(g(x)),
-            f(g(x)) => replace(h(x)),
-        ])
+        @rules Demo [x] begin
+            f(x)    := g(x)
+            f(g(x)) := h(x)
+        end
 
-        @test rewrite(rw, f(a())) == g(a())
-        @test rewrite(rw, f(g(a()))) ∈ [g(g(a())), h(a())]
-        @test rewrite(rw, g(a())) == g(a())
+        @test Demo(f(a())) == g(a())
+        @test Demo(f(g(a()))) ∈ [g(g(a())), h(a())]
+        @test Demo(g(a())) == g(a())
     end
 
     @testset "identical" begin
-        rw = Rules(:Demo, Example, [
-            f(x) => replace(g(x)),
-            f(x) => replace(h(x)),
-        ])
+        @rules Demo [x] begin
+            f(x) := g(x)
+            f(x) := h(x)
+        end
 
-        @test rewrite(rw, f(a())) ∈ [g(a()), h(a())]
-        @test rewrite(rw, f(f(a()))) ∈ [g(g(a())), g(h(a())), h(g(a())), h(h(a()))]
+        @test Demo(f(a())) ∈ [g(a()), h(a())]
+        @test Demo(f(f(a()))) ∈ [g(g(a())), g(h(a())), h(g(a())), h(h(a()))]
     end
 end
 
 @testset "commutative, simple" begin
-    lhs = p(x, x)
-    rhs = x
-    rw = Rules(:Demo, Example, [lhs => replace(rhs)])
-    @test rewrite(rw, p(a(), a())) == a()
-    @test rewrite(rw, p(b(), b())) == b()
-    @test rewrite(rw, p(a(), b())) == p(a(), b())
+    @rules Demo [x] begin
+        p(x, x) := x
+    end
 
-    rw = Rules(:Demo, Example)
-    push!(rw, lhs => replace(rhs))
-    @test rewrite(rw, p(a(), a())) == a()
+    @test Demo(p(a(), a())) == a()
+    @test Demo(p(b(), b())) == b()
+    @test Demo(p(a(), b())) == p(a(), b())
 end
 
 @testset "commutative, complex" begin
     @testset "orthogonal" begin
-        rw = Rules(:Demo, Example, [
-            p(a(), x)   => replace(x),
-            p(b(), b()) => replace(c()),
-        ])
+        @rules Demo [x] begin
+            p(a(), x)   := x
+            p(b(), b()) := c()
+        end
 
-        @test rewrite(rw, p(a(), b())) == b()
-        @test rewrite(rw, f(p(a(), b()))) == f(b())
+        @test Demo(p(a(), b())) == b()
+        @test Demo(f(p(a(), b()))) == f(b())
     end
 
     @testset "overlapping" begin
-        rw = Rules(:Demo, Example, [
-            p(a(), x) => replace(a()),
-            p(x, b()) => replace(b()),
-        ])
+        @rules Demo [x] begin
+            p(a(), x) := a()
+            p(x, b()) := b()
+        end
 
-        @test rewrite(rw, p(a(), c())) == a()
-        @test rewrite(rw, p(b(), c())) == b()
-        @test rewrite(rw, p(a(), b())) ∈ [a(), b()]
+        @test Demo(p(a(), c())) == a()
+        @test Demo(p(b(), c())) == b()
+        @test Demo(p(a(), b())) ∈ [a(), b()]
     end
 
     @testset "identical" begin
-        rw = Rules(:Demo, Example, [
-            p(x, y) => replace(q(x, x)),
-            q(x, x) => replace(a()),
-            q(x, x) => replace(b()),
-        ])
+        @rules Demo [x, y] begin
+            p(x, y) := q(x, x)
+            q(x, x) := a()
+            q(x, x) := b()
+        end
 
-        @test rewrite(rw, p(c(), c())) ∈ [a(), b()]
+        @test Demo(p(c(), c())) ∈ [a(), b()]
     end
 end
 
 
 @testset "sample cases" begin
     @testset "boolean logic" begin
-        @theory Bool begin
+        @theory! begin
             F   => FreeTheory()
             T   => FreeTheory()
             and => CTheory()
@@ -103,7 +102,7 @@ end
             not => FreeTheory()
         end
 
-        @rules Prop Bool [x, y] begin
+        @rules Prop [x, y] begin
             and(x, F) := F
             and(x, T) := x
             and(x, x) := x
@@ -114,29 +113,29 @@ end
 
             not(and(x, y)) := or(not(x), not(y))
             not(or(x, y))  := and(not(x), not(y))
-            not(not(x)) := x
+            not(not(x))    := x
         end
 
-        @test @rewrite(Prop, and(A, not(not(A)))) == @term(Bool, A)
-        @test @rewrite(Prop, and(or(F, F), B)) == @term(Bool, F)
+        @test @rewrite(Prop, and(A, not(not(A)))) == @term(A)
+        @test @rewrite(Prop, and(or(F, F), B)) == @term(F)
     end
 
     @testset "natural arithmetic" begin
-        @theory Nat begin
+        @theory! begin
             z => FreeTheory()
             s => FreeTheory()
             add => FreeTheory()
             mul => FreeTheory()
         end
 
-        @rules Arithmetic Nat [x, y] begin
+        @rules Arithmetic [x, y] begin
             add(x, z)    := x
             add(x, s(y)) := s(add(x, y))
             mul(x, z)    := z
             mul(x, s(y)) := add(x, mul(x, y))
         end
 
-        _nat(n) = n == 0 ? @term(Nat, z) : @term(Nat, s($(_nat(n - 1))))
+        _nat(n) = n == 0 ? @term(z) : @term(s($(_nat(n - 1))))
 
         @testset "$x * $y" for x ∈ 0:5, y ∈ 0:5
             nx, ny = _nat(x), _nat(y)
@@ -145,23 +144,23 @@ end
     end
 
     @testset "commutative addition" begin
-        @theory Nat begin
+        @theory! begin
             z => FreeTheory()
             s => FreeTheory()
             (+) => CTheory()
         end
 
-        @rules Addition Nat [x, y] begin
+        @rules Addition [x, y] begin
             x + z    := x
             s(x) + y := s(x + y)
         end
 
-        _nat(n) = n == 0 ? @term(Nat, z) : @term(Nat, s($(_nat(n - 1))))
+        _nat(n) = n == 0 ? @term(z) : @term(s($(_nat(n - 1))))
 
-        @test @term(Nat, a + b) == @term(Nat, b + a)
-        @test @rewrite(Addition, a + s(b)) == @term(Nat, s(a + b))
-        @test @rewrite(Addition, s(a) + b) == @term(Nat, s(a + b))
-        @test @rewrite(Addition, s(a) + s(b)) == @term(Nat, s(s(a + b)))
+        @test @term(a + b) == @term(b + a)
+        @test @rewrite(Addition, a + s(b)) == @term(s(a + b))
+        @test @rewrite(Addition, s(a) + b) == @term(s(a + b))
+        @test @rewrite(Addition, s(a) + s(b)) == @term(s(s(a + b)))
         @testset "$x + $y" for x ∈ 0:5, y ∈ 0:5
             nx, ny = _nat(x), _nat(y)
             @test @rewrite(Addition, $nx + $ny) == _nat(x + y)
@@ -169,30 +168,30 @@ end
     end
 
     @testset "list reverse" begin
-        @theory List begin
+        @theory! begin
             nil  => FreeTheory()
             cons => FreeTheory()
             rev     => FreeTheory()
             rev_aux => FreeTheory()
         end
 
-        @rules Reverse List [x, y, z] begin
+        @rules Reverse [x, y, z] begin
             rev_aux(nil, x)        := x
             rev_aux(cons(x, y), z) := rev_aux(y, cons(x, z))
             rev(x)                 := rev_aux(x, nil)
         end
 
         function _from(arr)
-            l = @term(List, nil)
+            l = @term(nil)
             for x ∈ reverse(arr)
-                l = @term(List, cons($x, $l))
+                l = @term(cons($x, $l))
             end
             return l
         end
 
         @testset "reverse length $n" for n ∈ 0:10
             local list
-            arr = [@term(List, $(Symbol(i))) for i ∈ 1:n]
+            arr = [@term($(Symbol(i))) for i ∈ 1:n]
             @test @rewrite(Reverse, rev($(_from(arr)))) == _from(reverse(arr))
         end
     end
